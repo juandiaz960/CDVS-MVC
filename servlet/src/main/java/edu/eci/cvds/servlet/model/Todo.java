d6d90f61d8694d48351ebccf46a136b7b4cc2ec7
juandiaz960 <42452344+juandiaz960@users.noreply.github.com>
2020-03-03 14:51:54 -0500

    Parte 2 Terminada

---
+++ servlet/src/main/java/edu/eci/cvds/servlet/model/Todo.java
@@ -0,0 +1,48 @@
+package edu.eci.cvds.servlet.model;
+
+/**
+ *
+ * @author Juan Diaz
+ */
+public class Todo {
+    int userId;
+    int id;
+    String title;
+    boolean completed;
+
+    public Todo() {
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean getCompleted() {
+        return completed;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+    
+}
